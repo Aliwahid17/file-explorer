f4e7c1ab6a591babf81696669fb5dc1a6f2f58ef
wahid ali <88263575+Aliwahid17@users.noreply.github.com>
2023-06-04 09:43:22 +0530

    created models

--- explorer/admin.py
+++ explorer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Folder , File
 
 # Register your models here.
+
+admin.site.register(Folder)
+admin.site.register(File)
--- explorer/models.py
+++ explorer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Folder(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+class File(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey(Folder, on_delete=models.CASCADE)
+
+
+# root_folder = Folder.objects.create(name='root')
+# sub_folder = Folder.objects.create(name='subfolder', parent=root_folder)
