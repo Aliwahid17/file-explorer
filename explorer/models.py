fc626b1978ae07149da1dc2a8be4dd75362d3caa
wahid ali <88263575+Aliwahid17@users.noreply.github.com>
2023-06-06 16:51:10 +0530

    backend

--- explorer/models.py
+++ explorer/models.py
@@ -1,15 +1,59 @@
 from django.db import models
+import re
+from os.path import join
 
 # Create your models here.
 
+
 class Folder(models.Model):
     name = models.CharField(max_length=255)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    # def path_name(self):
+    #     pass
+
 
 class File(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, default="File Name")
     parent = models.ForeignKey(Folder, on_delete=models.CASCADE)
+
+    def get_upload_path(self, filename):
+        parent = self.parent
+        path = []
+
+        while parent is not None:
+            path.append(str(parent))
+            parent = parent.parent
+
+        return join(*reversed(path), filename)
+
+    file = models.FileField(upload_to=get_upload_path)
+
+    def __str__(self):
+        return self.name
+
+    # def path_name(self):
+    #     parent = self.parent
+    #     path = []
+
+    #     while parent is not None:
+    #         path.append(str(parent))
+    #         parent = parent.parent
+
+    #     # return path
+    #     return join(*reversed(path))
+
+    def save(self, *args, **kwargs):
+        self.name = re.search(r"[^/]+$", str(self.file)).group()
+        super().save(*args, **kwargs)
 
 
 # root_folder = Folder.objects.create(name='root')
+# sub_folder = Folder.objects.create(name='subfolder', parent=root_folder)\
+
+# root_folder = Folder.objects.create(name='root')
 # sub_folder = Folder.objects.create(name='subfolder', parent=root_folder)
+# sub_sub_folder = Folder.objects.create(name='subsubfolder', parent=sub_folder)
--- explorer/views.py
+++ explorer/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render , HttpResponse
+from .models import Folder , File
+import re
 
 # Create your views here.
 
 def home(request):
+
+
+
+    # print(File.objects.all()[0].file, type(File.objects.all()[0].file))
+
+    # print(re.search(r'[^/]+$', str(File.objects.all()[0].file)).group())
+
+    # # with open(str(File.objects.all()[0].file) , 'r') as f:
+    # #     print(f.read()) 
+
+    # print(File.objects.all()[1].parent.parent)
+
+    # print('v',File.objects.all()[1].path_name())
+
     return HttpResponse('<h1>Hello World</h1>')
