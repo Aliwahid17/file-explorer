6468100e9a53c2409c84f9e2a32258d5203ede19
wahid ali <88263575+Aliwahid17@users.noreply.github.com>
2023-06-06 18:38:20 +0530

    db modeling is done

--- explorer/models.py
+++ explorer/models.py
@@ -4,17 +4,12 @@ from os.path import join
 
 # Create your models here.
 
-
 class Folder(models.Model):
     name = models.CharField(max_length=255)
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-    # def path_name(self):
-    #     pass
-
 
 class File(models.Model):
     name = models.CharField(max_length=255, default="File Name")
@@ -27,24 +22,13 @@ class File(models.Model):
         while parent is not None:
             path.append(str(parent))
             parent = parent.parent
-
-        return join(*reversed(path), filename)
+        
+        return f"data/{join(*reversed(path), filename)}"
 
     file = models.FileField(upload_to=get_upload_path)
 
     def __str__(self):
         return self.name
-
-    # def path_name(self):
-    #     parent = self.parent
-    #     path = []
-
-    #     while parent is not None:
-    #         path.append(str(parent))
-    #         parent = parent.parent
-
-    #     # return path
-    #     return join(*reversed(path))
 
     def save(self, *args, **kwargs):
         self.name = re.search(r"[^/]+$", str(self.file)).group()
--- explorer/views.py
+++ explorer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render
 from .models import Folder , File
 import re
 
@@ -6,17 +6,4 @@ import re
 
 def home(request):
 
-
-
-    # print(File.objects.all()[0].file, type(File.objects.all()[0].file))
-
-    # print(re.search(r'[^/]+$', str(File.objects.all()[0].file)).group())
-
-    # # with open(str(File.objects.all()[0].file) , 'r') as f:
-    # #     print(f.read()) 
-
-    # print(File.objects.all()[1].parent.parent)
-
-    # print('v',File.objects.all()[1].path_name())
-
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request,'routes/index.html')
